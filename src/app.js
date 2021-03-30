a7c33044e7945cef3139064dbe8ccdeec40f700a
Gabriel Demamann <gabrieldemamann@gmail.com>
2021-03-30 09:08:37 -0300

    Added helmet and cors, updated .env.example

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 import './config/dotenv';
 import './database/mongodb';
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 import homeRoutes from './routes/home';
 import userRoutes from './routes/user';
 import authRoutes from './routes/auth';
 import recoveryRoutes from './routes/recovery';
+
+const whiteList = [`'${process.env.API_DOMAIN}'`];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not alloewd by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -16,6 +30,8 @@ class App {
   middlewares() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
+    this.app.use(helmet());
+    this.app.use(cors(corsOptions));
   }
 
   routes() {
